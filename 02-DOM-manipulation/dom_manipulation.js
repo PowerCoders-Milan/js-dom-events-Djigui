288c936804756e3717b609aab964521140d70e1a
Djigui <djigui@1F-01.edu-il.it>
2021-06-28 18:07:19 +0200

    seconda esercizio e da completare

--- 02-DOM-manipulation/dom_manipulation.js
+++ 02-DOM-manipulation/dom_manipulation.js
@@ -1,9 +1,9 @@
 // Ex 1. Add the .blue CSS class to the th elements
 
-const thElements = document.querySelectorAll('');
+const thElements = document.querySelectorAll('th');
 
 thElements.forEach((element) => {
-  element.className = "";
+  element.className = "th";
 });
 
 // Ex 2. Count the number of table body rows there are
@@ -11,29 +11,29 @@ thElements.forEach((element) => {
 const teamCount = () => {
   // TODO: return the number of teams
   const trElements = document.querySelectorAll('');
-  return ;
+  return tr;
 };
 
 // Ex 3. Say there is a 15th team added to the table.
 //       Add a row at the bottom, this new team (Spezia) should have 39 points.
 
-const list = document.querySelector('');
-list.insertAdjacentHTML('beforeend', '');
+const list = document.querySelector('th');
+list.insertAdjacentHTML('beforeend', 'td');
 
 // Ex 4. Write some code to sum all points given to all teams
 //       Make the function summarizePoints() return it
 const summarizePoints = () => {
   // TODO: return the sum
-  return ;
+  return th;
 };
 
 
 // Ex 5. Change the background color of all `<th>` cells to #DDF4FF
 
-const thEl = document.querySelectorAll('');
+const thEl = document.querySelectorAll('.blue');
 
 thEl.forEach((element) => {
-  element.style.backgroundColor = "";
+  element.style.backgroundColor = "blue";
 });
 
 
